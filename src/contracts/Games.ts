ab681e7d7bc8fac2c6f874377cb262b6406e5f4f
Aneel Kumar <gakaneelkumar1997@gmail.com>
2020-05-20 15:20:02 +0530

    Added default gh-files for first publish

--- src/contracts/Games.ts
+++
@@ -1,5 +0,0 @@
-import { IGame } from "../concerns/Game";
-
-export interface IGamesContract {
-    get: () => Promise<IGame[]>;
-}
--- src/contracts/Http.ts
+++
@@ -1,3 +0,0 @@
-export interface IHttpContract {
-    get: (url: string) => Promise<any>;
-}
